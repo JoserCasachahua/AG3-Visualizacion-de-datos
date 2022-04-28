f4990dc3bb94b9d2d1e8bfe5d1a5b32ef98219a2
JoserCasachahua <jcasachahuam@gmail.com>
2022-04-27 23:11:51 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Import stylesheets
 import './style.css';
+
+var data = [{"year": 1991, "name":"alpha", "value": 15},{"year": 1992,"name":"alpha", "value": 20},];
+
+//Gráfico de columnas. Mi 1º gráfico 
+var visualization = d3plus.viz()    
+    .container("#viz1")    
+    .data(data)    
+    .type("bar")    
+    .id("name")    
+    .x("year")    
+    .y("value")    
+    .draw()
